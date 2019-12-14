a186df091668bc400d1242e96dace6a754a43d66
David <David.Camunas2@alu.uclm.es>
2019-12-14 12:22:12 +0100

    Creacion divisiones Ventana Principal

--- GCT_IPO1/src/presentacion/VentanaPrincipal.java
+++ GCT_IPO1/src/presentacion/VentanaPrincipal.java
@@ -9,12 +9,32 @@ import javax.swing.border.EmptyBorder;
 import java.awt.Toolkit;
 import javax.swing.JToolBar;
 import java.awt.Dimension;
+import javax.swing.JButton;
+import javax.swing.SwingConstants;
+import javax.swing.border.BevelBorder;
+import java.awt.FlowLayout;
+import java.awt.GridLayout;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.ImageIcon;
+import java.awt.GridBagLayout;
+import javax.swing.JTabbedPane;
+import javax.swing.JLabel;
+import java.awt.CardLayout;
 
 public class VentanaPrincipal extends JFrame {
 
 	private JPanel contentPane;
-	private JToolBar toolBar;
-	private JPanel panel;
+	private JPanel pnHerramientas;
+	private JPanel pnContenido;
+	private JButton btnCerrarSesion;
+	private JButton btnAyuda;
+	private JTabbedPane tbdPestanias;
+	private JPanel pnCircuitos;
+	private JLabel lblUsuario;
+	private JPanel pnGuias;
+	private JPanel pnGrupos;
+	private JPanel pnDisenioRuta;
 
 	/**
 	 * Launch the application.
@@ -45,12 +65,56 @@ public class VentanaPrincipal extends JFrame {
 		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
 		
-		toolBar = new JToolBar();
-		toolBar.setPreferredSize(new Dimension(0, 30));
-		contentPane.add(toolBar, BorderLayout.NORTH);
+		pnHerramientas = new JPanel();
+		pnHerramientas.setBorder(new BevelBorder(BevelBorder.LOWERED, null, null, null, null));
+		pnHerramientas.setPreferredSize(new Dimension(0, 40));
+		contentPane.add(pnHerramientas, BorderLayout.NORTH);
+		pnHerramientas.setLayout(new FlowLayout(FlowLayout.RIGHT, 5, 5));
 		
-		panel = new JPanel();
-		contentPane.add(panel, BorderLayout.CENTER);
+		btnCerrarSesion = new JButton("Cerrar Sesion");
+		btnCerrarSesion.addActionListener(new BtnNewButtonActionListener());
+		
+		lblUsuario = new JLabel("Usuario Inicio");
+		pnHerramientas.add(lblUsuario);
+		
+		btnAyuda = new JButton("");
+		btnAyuda.setIcon(new ImageIcon(VentanaPrincipal.class.getResource("/presentacion/imagenes/iconos/info.png")));
+		btnAyuda.setContentAreaFilled(false);
+		btnAyuda.setBorderPainted(false);
+		pnHerramientas.add(btnAyuda);
+		pnHerramientas.add(btnCerrarSesion);
+		
+		pnContenido = new JPanel();
+		contentPane.add(pnContenido, BorderLayout.CENTER);
+		pnContenido.setLayout(new BorderLayout(0, 0));
+		
+		tbdPestanias = new JTabbedPane(JTabbedPane.TOP);
+		tbdPestanias.setToolTipText("");
+		pnContenido.add(tbdPestanias);
+		
+		pnCircuitos = new JPanel();
+		tbdPestanias.addTab("Circuitos", null, pnCircuitos, null);
+		GridBagLayout gbl_pnCircuitos = new GridBagLayout();
+		gbl_pnCircuitos.columnWidths = new int[]{0};
+		gbl_pnCircuitos.rowHeights = new int[]{0};
+		gbl_pnCircuitos.columnWeights = new double[]{Double.MIN_VALUE};
+		gbl_pnCircuitos.rowWeights = new double[]{Double.MIN_VALUE};
+		pnCircuitos.setLayout(gbl_pnCircuitos);
+		
+		pnGuias = new JPanel();
+		tbdPestanias.addTab("Guias", null, pnGuias, null);
+		
+		pnGrupos = new JPanel();
+		tbdPestanias.addTab("Grupos", null, pnGrupos, null);
+		
+		pnDisenioRuta = new JPanel();
+		tbdPestanias.addTab("Diseño Ruta", null, pnDisenioRuta, null);
 	}
 
+	private class BtnNewButtonActionListener implements ActionListener {
+		public void actionPerformed(ActionEvent e) {
+			// Se cierra la aplicacion
+			System.exit(0);
+		}
+	}
 }
