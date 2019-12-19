d804d596ff18b15335e684c8fa771b0764de23f5
David <David.Camunas2@alu.uclm.es>
2019-12-19 10:26:24 +0100

    Creadas clases paquete dominio
    √ Circuito
    √ Usuario
    √ Lugar
    √ Turista

---
+++ GCT_IPO1/src/dominio/Usuario.java
@@ -0,0 +1,40 @@
+package dominio;
+
+public class Usuario {
+	
+	private String usuario;
+	private String nombre;
+	private String correo;
+	
+	public Usuario(String usuario, String nombre, String correo) {
+		this.usuario = usuario;
+		this.nombre = nombre;
+		this.correo = correo;
+	}
+
+	public String getUsuario() {
+		return usuario;
+	}
+
+	public void setUsuario(String usuario) {
+		this.usuario = usuario;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getCorreo() {
+		return correo;
+	}
+
+	public void setCorreo(String correo) {
+		this.correo = correo;
+	}
+	
+
+}
