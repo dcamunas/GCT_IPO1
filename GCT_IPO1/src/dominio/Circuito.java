afa579caf2b0854f2806e10a3e8ef9506daaa315
David <David.Camunas2@alu.uclm.es>
2019-12-19 10:26:43 +0100

    Creada clase circuito

--- GCT_IPO1/src/dominio/Circuito.java
+++ GCT_IPO1/src/dominio/Circuito.java
@@ -1,5 +1,96 @@
 package dominio;
 
+import java.util.*;
+
 public class Circuito {
+	
+	private int id = -1;					//Usado en la bd
+	private String nombre_circuito;
+	private int personas_realizado;
+	private String precio;			// Castear a entero
+	private boolean incidencia;
+	private ArrayList<String> incidencias_lista;
+	private ArrayList<String> puntos_interes;
+	private ArrayList<String> sugerencias;
+	private ArrayList<Lugar> lugares;
+	private boolean contratado;
+	
+	
+	
+	
+	public Circuito(String nombre_circuito, int personas_realizado, String precio, boolean incidencia,
+			ArrayList<String> incidencias_lista, ArrayList<String> puntos_interes, ArrayList<String> sugerencias,
+			ArrayList<Lugar> lugares, boolean contratado) {
+		this.id++;
+		this.nombre_circuito = nombre_circuito;
+		this.personas_realizado = personas_realizado;
+		this.precio = precio;
+		this.incidencia = incidencia;
+		this.incidencias_lista = incidencias_lista;
+		this.puntos_interes = puntos_interes;
+		this.sugerencias = sugerencias;
+		this.lugares = lugares;
+		this.contratado = contratado;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public String getNombre_circuito() {
+		return nombre_circuito;
+	}
+
+	public int getPersonas_realizado() {
+		return personas_realizado;
+	}
+	public void setPersonas_realizado(int personas_realizado) {
+		this.personas_realizado = personas_realizado;
+	}
+	public String getPrecio() {
+		return precio;
+	}
+
+	public boolean getIncidencia() {
+		return incidencia;
+	}
+	public void setIncidencia(boolean incidencia) {
+		this.incidencia = incidencia;
+	}
+	public ArrayList<String> getIncidencias_lista() {
+		return incidencias_lista;
+	}
+	public void setIncidencias_lista(ArrayList<String> incidencias_lista) {
+		this.incidencias_lista = incidencias_lista;
+	}
+	public ArrayList<String> getPuntos_interes() {
+		return puntos_interes;
+	}
+	public void setPuntos_interes(ArrayList<String> puntos_interes) {
+		this.puntos_interes = puntos_interes;
+	}
+	public ArrayList<String> getSugerencias() {
+		return sugerencias;
+	}
+	public void setSugerencias(ArrayList<String> sugerencias) {
+		this.sugerencias = sugerencias;
+	}
+	public ArrayList<Lugar> getLugares() {
+		return lugares;
+	}
+	public void setLugares(ArrayList<Lugar> lugares) {
+		this.lugares = lugares;
+	}
+	public boolean getContratado() {
+		return contratado;
+	}
+	public void setContratado(boolean contratado) {
+		this.contratado = contratado;
+	}
+	
+	
+	
+	
+
 
 }
